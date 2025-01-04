3c5fac40fc006dbfcdae996055660a4f69184cab
p_buian <p.buyan@gmail.com>
2025-01-03 19:03:43 -0500

    synatx highlighter

--- ui/code-highlighter.tsx
+++ ui/code-highlighter.tsx
@@ -1,19 +1,26 @@
-import { Component } from 'react';
-import * as fs from 'fs';
-import path from 'path';
-import { Prism, SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import type { Component } from "react";
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import * as fs from "fs";
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import path from "path";
+import { Prism, type SyntaxHighlighterProps } from "react-syntax-highlighter";
 const SyntaxHighlighter = Prism as typeof Component<SyntaxHighlighterProps>;
-import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const CodeHighlighter = ({ filePath }: { filePath: string }) => {
-  const fPath = path.join(process.cwd(), 'data', filePath);
-  const code = fs.readFileSync(fPath, 'utf-8');
+	const fPath = path.join(process.cwd(), "data", filePath);
+	const code = fs.readFileSync(fPath, "utf-8");
 
-  return (
-    <SyntaxHighlighter style={darcula} language="javascript" code={code}>
-      {code}
-    </SyntaxHighlighter>
-  );
+	return (
+		<SyntaxHighlighter
+			style={materialDark}
+			language="jsx"
+			code={code}
+			showLineNumbers={true}
+		>
+			{code}
+		</SyntaxHighlighter>
+	);
 };
 
 export default CodeHighlighter;
